cde8e6c1090b01023dc9ec49c5e127acb5d71d61
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-03-18 11:21:41 -0300

    Add fluxo de persistencia do objeto de retorno do webhook

--- src/main/java/br/com/meetime/hubspotintegration/controller/WebhookController.java
+++ src/main/java/br/com/meetime/hubspotintegration/controller/WebhookController.java
@@ -1,6 +1,7 @@
 package br.com.meetime.hubspotintegration.controller;
 
 import br.com.meetime.hubspotintegration.model.WebhookEvent;
+import br.com.meetime.hubspotintegration.service.ContactService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +16,15 @@ import java.util.List;
 @RequestMapping("/webhook")
 public class WebhookController {
 
-    public WebhookController() {
+    private final ContactService contactService;
+
+    public WebhookController(ContactService contactService) {
+        this.contactService = contactService;
     }
 
     @PostMapping("/contact-creation")
     public ResponseEntity<String> handleContactCreation(@RequestBody List<WebhookEvent> events) throws URISyntaxException {
+        contactService.saveContacts(events);
         return ResponseEntity.created(new URI("/webhook/contact-creation")).body("Webhook recebido com sucesso!");
     }
 }
