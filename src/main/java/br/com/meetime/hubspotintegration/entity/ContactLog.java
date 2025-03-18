cc1a108b2325d41e2a39a8edda7bb5c87b9e3618
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-03-18 12:43:02 -0300

    Config para o mysql

--- src/main/java/br/com/meetime/hubspotintegration/entity/ContactLog.java
+++ src/main/java/br/com/meetime/hubspotintegration/entity/ContactLog.java
@@ -5,21 +5,43 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "contact_log")
+@Table(name = "contactlog")
 public class ContactLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "objectid")
     private Long objectId;
+
+    @Column(name = "propertyname")
     private String propertyName;
+
+    @Column(name = "propertyvalue")
     private String propertyValue;
+
+    @Column(name = "changesource")
     private String changeSource;
+
+    @Column(name = "eventid")
     private Long eventId;
+
+    @Column(name = "subscriptionid")
     private Integer subscriptionId;
+
+    @Column(name = "portalid")
     private Integer portalId;
+
+    @Column(name = "appid")
     private Integer appId;
+
+    @Column(name = "occurredat")
     private Long occurredAt;
+
+    @Column(name = "eventtype")
     private String eventType;
+
+    @Column(name = "attemptnumber")
     private Integer attemptNumber;
 }
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS contact_log (
+CREATE TABLE IF NOT EXISTS contactlog (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    object_id BIGINT NOT NULL,
-    property_name VARCHAR(255) NOT NULL,
-    property_value VARCHAR(255),
-    change_source VARCHAR(255),
-    event_id BIGINT,
-    subscription_id INT,
-    portal_id INT,
-    app_id INT,
-    occurred_at BIGINT,
-    event_type VARCHAR(255),
-    attempt_number INT
+    objectid BIGINT NOT NULL,
+    propertyname VARCHAR(255) NOT NULL,
+    propertyvalue VARCHAR(255),
+    changesource VARCHAR(255),
+    eventid BIGINT,
+    subscriptionid INT,
+    portalid INT,
+    appid INT,
+    occurredat BIGINT,
+    eventtype VARCHAR(255),
+    attemptnumber INT
     );
