b0b38a1da20bccb4d31fb63e0b40d02352a1b53f
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-03-18 17:46:50 -0300

    Ajusta teste main

--- src/test/java/br/com/meetime/hubspotintegration/HubspotIntegrationApplicationTest.java
+++ src/test/java/br/com/meetime/hubspotintegration/HubspotIntegrationApplicationTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
@@ -13,7 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class HubspotIntegrationApplicationTest {
 
     @Test
-    void applicationStartsSuccessfully() {
-        assertDoesNotThrow(() -> SpringApplication.run(HubspotIntegrationApplication.class));
+    void applicationContextLoads() {
+        HubspotIntegrationApplication.main(new String[] {});
+        assertTrue(true);
     }
 }
