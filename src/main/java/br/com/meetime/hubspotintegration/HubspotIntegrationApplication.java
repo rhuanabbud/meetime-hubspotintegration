4d98c0acaccd198ffb1cb5be48971f1b42af15f2
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-03-18 00:22:38 -0300

    Renomeação da classe principal spring para padrão do projeto.

--- src/main/java/br/com/meetime/hubspotintegration/HubspotIntegrationApplication.java
+++ src/main/java/br/com/meetime/hubspotintegration/HubspotIntegrationApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class HubspotIntegrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(HubspotIntegrationApplication.class, args);
     }
-
 }
