f544b692e1c70d1b672d7f35a06da7a08bb542f1
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-03-18 14:42:32 -0300

    Ajuste dos objetos segundo API do hubspot

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,14 +1,11 @@
 CREATE TABLE IF NOT EXISTS contactlog (
     id INT AUTO_INCREMENT PRIMARY KEY,
     objectid BIGINT NOT NULL,
-    propertyname VARCHAR(255) NOT NULL,
-    propertyvalue VARCHAR(255),
     changesource VARCHAR(255),
     eventid BIGINT,
     subscriptionid INT,
     portalid INT,
     appid INT,
     occurredat BIGINT,
-    eventtype VARCHAR(255),
     attemptnumber INT
     );
